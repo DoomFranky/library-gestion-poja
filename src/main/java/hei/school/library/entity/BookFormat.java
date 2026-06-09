2995e41c163c09b14365311d18f2fcfadc6b684f
Mahefa04 <hei.mahefa.2@gmail.com>
2026-06-09 22:01:53 +0300

    feat: add BookFormatRepository

--- src/main/java/hei/school/library/entity/BookFormat.java
+++ src/main/java/hei/school/library/entity/BookFormat.java
@@ -15,4 +15,25 @@ public class BookFormat {
     @Id
     private Integer id;
     private String label;
+
+    public BookFormat(Integer id, String label) {
+        this.id = id;
+        this.label = label;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
 }
