e107af1b10a6534199bb3f51e948b8ac601d1b88
nassaigael <hei.nassai@gmail.com>
2026-06-17 16:16:17 +0300

    feat(repository): implement AuthorRepository interface
    * extend AuthorRepository to inherit from JpaRepository
    * add methods to find authors by name and ID
    
    This change establishes the AuthorRepository interface for
    interacting with Author entities in the database, enabling
    CRUD operations and improving data retrieval capabilities.

--- src/main/java/hei/school/library/repository/AuthorRepository.java
+++ src/main/java/hei/school/library/repository/AuthorRepository.java
@@ -1,4 +1,10 @@
 package hei.school.library.repository;
 
-public interface AuthorRepository {
+import hei.school.library.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, String> {
+    Author findByname(String name);
+    Author findAuthorsById(String id);
+
 }
