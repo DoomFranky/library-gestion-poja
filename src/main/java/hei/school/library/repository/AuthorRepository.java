f599cc3fcc7485e3ec82f78fe786924575f9486f
nassaigael <hei.nassai@gmail.com>
2026-06-17 16:19:28 +0300

    refactor(repository): remove unnecessary blank line
    * Removed an extra blank line at the end of the AuthorRepository interface
    * This change improves code readability and adheres to standard formatting

--- src/main/java/hei/school/library/repository/AuthorRepository.java
+++ src/main/java/hei/school/library/repository/AuthorRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AuthorRepository extends JpaRepository<Author, String> {
     Author findByname(String name);
     Author findAuthorsById(String id);
-
 }
