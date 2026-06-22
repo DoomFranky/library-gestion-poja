9df544c6abafc3d20563ff3eb1342fae76dfc243
nomena <hei.nomenjanahary@gmail.com>
2026-06-22 08:17:30 +0300

    add a test shouldReturnBookCopyById

---
+++ src/test/java/hei/school/library/controller/BookCopyControllerTest.java
@@ -0,0 +1,48 @@
+package hei.school.library.controller;
+
+import hei.school.library.entity.BookCopy;
+import hei.school.library.entity.BookFormat;
+import hei.school.library.entity.PriceBook;
+import hei.school.library.service.BookCopyService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookCopyController.class)
+public class BookCopyControllerTest {
+    private BookCopy harryPotter;
+
+    @MockBean private BookCopyService bookCopyService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        harryPotter = new BookCopy();
+        harryPotter.setId("123");
+    }
+
+
+    @Test
+    void shouldReturnBookCopyById()throws Exception {
+    when(bookCopyService.findBookCopyById("123")).thenReturn(harryPotter);
+    mockMvc.perform(get("/books/copy/123")).andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value("123"));
+
+        verify(bookCopyService)
+                .findBookCopyById("123");
+
+    }
+}
