7745ddbaa5d93261f4d187905a441c3efc60bfc2
Randy <raoulrandy3@gmail.com>
2026-07-10 00:13:01 +0300

    style(stock): apply format

--- src/main/java/hei/school/library/repository/StockMovementRepository.java
+++ src/main/java/hei/school/library/repository/StockMovementRepository.java
@@ -10,22 +10,16 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StockMovementRepository extends JpaRepository<StockMovement, String> {
   @Query(
-      "SELECT SUM("
-          + "CASE WHEN sm.movementTypeEnum= hei.school.library.entity.MovementTypeEnum.OUT THEN sm.quantity * -1 ELSE sm.quantity END"
-          + ") ,b.title, bf.format FROM Book b "
-          + "JOIN b.bookFormat bf "
-          + "JOIN BookCopy bc ON bc.bookFormat = bf "
-          + "JOIN StockMovement sm ON sm.bookCopy = bc "
-          + "WHERE b.id = :bookId ORDER BY (b.title, bf.format)")
+      "SELECT SUM(CASE WHEN sm.movementTypeEnum= hei.school.library.entity.MovementTypeEnum.OUT"
+          + " THEN sm.quantity * -1 ELSE sm.quantity END) ,b.title, bf.format FROM Book b JOIN"
+          + " b.bookFormat bf JOIN BookCopy bc ON bc.bookFormat = bf JOIN StockMovement sm ON"
+          + " sm.bookCopy = bc WHERE b.id = :bookId ORDER BY (b.title, bf.format)")
   StockQuantityDTO findQuantityOfBookById(@Param("bookId") String id);
 
   @Query(
-      "SELECT SUM("
-          + "CASE WHEN sm.movementTypeEnum= hei.school.library.entity.MovementTypeEnum.OUT THEN sm.quantity * -1 ELSE sm.quantity END "
-          + "), b.title,bf.format FROM Book b "
-          + "JOIN b.bookFormat bf "
-          + "JOIN BookCopy bc ON bc.bookFormat = bf "
-          + "JOIN StockMovement sm ON sm.bookCopy = bc "
-          + "WHERE bc.id = :bookCopyId ORDER BY (b.title,bf.format)")
+      "SELECT SUM(CASE WHEN sm.movementTypeEnum= hei.school.library.entity.MovementTypeEnum.OUT"
+          + " THEN sm.quantity * -1 ELSE sm.quantity END ), b.title,bf.format FROM Book b JOIN"
+          + " b.bookFormat bf JOIN BookCopy bc ON bc.bookFormat = bf JOIN StockMovement sm ON"
+          + " sm.bookCopy = bc WHERE bc.id = :bookCopyId ORDER BY (b.title,bf.format)")
   StockQuantityDTO findQuantityOfBookCopyById(@Param("bookCopyId") String id);
 }
